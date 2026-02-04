91f02b67f8b965bd8e8bb37526af22e2875e53ee
Sophia <sophie@soap-phia.dev>
2026-02-04 13:07:46 -0500

    icl i forget what i did aside from add verbosity

--- main.go
+++ main.go
@@ -60,7 +60,9 @@ func hexToHsl(hex string) (int, int, int) {
 		switch max {
 		case red:
 			h = (green - blue) / delta
-			if green < blue { h += 6 }
+			if green < blue {
+				h += 6
+			}
 		case green:
 			h = (blue - red) / delta + 2
 		case blue:
@@ -71,7 +73,7 @@ func hexToHsl(hex string) (int, int, int) {
 	return int(h * 360), int(s * 100), int(l * 100)
 }
 
-func process(path string, catpuccin map[string][]string, target map[string]string) error {
+func process(path string, catpuccin map[string][]string, target map[string]string, verbose bool) error {
 	input, err := os.ReadFile(path)
 	if err != nil {
 		return err
@@ -79,6 +81,13 @@ func process(path string, catpuccin map[string][]string, target map[string]strin
 
 	output := string(input)
 	original := output
+
+	type changeRecord struct {
+		old string
+		new string
+	}
+	var changes []changeRecord
+	newColors := make(map[string]bool)
 
 	for color, values := range catpuccin {
 		newHex, exists := target[color]
@@ -93,20 +102,40 @@ func process(path string, catpuccin map[string][]string, target map[string]strin
 		newRgbArray := fmt.Sprintf("[%d, %d, %d]", r, g, b)
 		newHsl := fmt.Sprintf("hsl(%ddeg, %d%%, %d%%)", h, s, l)
 
+		newColors[strings.ToLower(strings.ReplaceAll(newHex, " ", ""))] = true
+		newColors[strings.ToLower(strings.ReplaceAll(newRgb, " ", ""))] = true
+		newColors[strings.ToLower(strings.ReplaceAll(newRgbArray, " ", ""))] = true
+		newColors[strings.ToLower(strings.ReplaceAll(newHsl, " ", ""))] = true
+
 		for _, value := range values {
-			if strings.HasPrefix(value, "#") {
-				output = strings.ReplaceAll(output, value, newHex)
-			} else if strings.HasPrefix(value, "rgb") {
-				output = strings.ReplaceAll(output, value, newRgb)
-				var rv, gv, bv int
-				_, err := fmt.Sscanf(value, "rgb(%d, %d, %d)", &rv, &gv, &bv)
-				if err == nil {
-					pattern := fmt.Sprintf(`\[\s*%d\s*,\s*%d\s*,\s*%d\s*\]`, rv, gv, bv)
-					regex := regexp.MustCompile(pattern)
-					output = regex.ReplaceAllString(output, newRgbArray)
+			escCode := regexp.QuoteMeta(value)
+			regexPattern := strings.ReplaceAll(escCode, `\ `, `\s+`)
+			regexPattern = strings.ReplaceAll(regexPattern, `\n`, `\s*`)
+
+			reg := regexp.MustCompile(regexPattern)
+			if reg.MatchString(output) {
+				replacement := ""
+				if strings.HasPrefix(value, "#") {
+					replacement = newHex
+				} else if strings.HasPrefix(value, "rgb") {
+					replacement = newRgb
+					var rv, gv, bv int
+					_, err := fmt.Sscanf(value, "rgb(%d, %d, %d)", &rv, &gv, &bv)
+					if err == nil {
+						pattern := fmt.Sprintf(`\[\s*%d\s*,\s*%d\s*,\s*%d\s*\]`, rv, gv, bv)
+						regex := regexp.MustCompile(pattern)
+						if regex.MatchString(output) {
+							output = regex.ReplaceAllString(output, newRgbArray)
+						}
+					}
+				} else if strings.HasPrefix(value, "hsl") {
+					replacement = newHsl
 				}
-			} else if strings.HasPrefix(value, "hsl") {
-				output = strings.ReplaceAll(output, value, newHsl)
+
+				if replacement != "" {
+					output = reg.ReplaceAllString(output, replacement)
+					changes = append(changes, changeRecord{old: value, new: replacement})
+				}
 			}
 		}
 	}
@@ -115,6 +144,21 @@ func process(path string, catpuccin map[string][]string, target map[string]strin
 		err = os.WriteFile(path, []byte(output), 0644)
 		if err == nil {
 			fmt.Printf("Modified %s\n", filepath.Base(path))
+			if verbose {
+				for _, c := range changes {
+					fmt.Printf("  \033[32m%s -> %s\033[0m\n", c.old, c.new)
+				}
+				colorRegex := regexp.MustCompile(`(#[A-Fa-f0-9]{3,6}|rgb\([^\)]+\)|hsl\([^\)]+\)|\[\s*\d+\s*,\s*\d+\s*,\s*\d+\s*\])`)
+				remaining := colorRegex.FindAllString(output, -1)
+				seen := make(map[string]bool)
+				for _, found := range remaining {
+					normalized := strings.ToLower(strings.ReplaceAll(found, " ", ""))
+					if !newColors[normalized] && !seen[normalized] {
+						fmt.Printf("  \033[31m%s\033[0m\n", found)
+						seen[normalized] = true
+					}
+				}
+			}
 		}
 		return err
 	}
@@ -125,6 +169,7 @@ func main() {
 	path := flag.String("path", "", "File/Dir to modify")
 	newTheme := flag.String("theme", "", "The theme file")
 	catpuccinType := flag.String("type", "macchiato", "The catppucin theme you're replacing")
+	verbose := flag.Bool("verbose", false, "List changed and unchanged colors")
 	flag.Parse()
 
 	if *path == "" || *newTheme == "" {
@@ -169,7 +214,7 @@ func main() {
 		if info.IsDir() {
 			return nil
 		}
-		return process(path, catpuccin, target)
+		return process(path, catpuccin, target, *verbose)
 	})
 
 	if err != nil {
